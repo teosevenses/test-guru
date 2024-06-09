e99b51a1b3123effcbd7b718dbd5cec8adbd211a
teosevenses <badlev@mail.ru>
2024-06-09 18:56:15 +0500

    Error correction 2.0

--- test/controllers/static_pages1_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class StaticPages1ControllerTest < ActionDispatch::IntegrationTest
-  test "should get about_author" do
-    get static_pages1_about_author_url
-    assert_response :success
-  end
-end
